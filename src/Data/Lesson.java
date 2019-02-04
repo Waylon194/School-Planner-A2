cf7068eb97e65acc031046f9f9c7668e537790e2
Vincent <vwjapderooij@gmail.com>
2019-02-04 12:40:32 +0100

    Added functionality to the Lesson class

--- src/Data/Lesson.java
+++ src/Data/Lesson.java
@@ -1,5 +1,58 @@
 package Data;
 
+import java.util.ArrayList;
+
 public class Lesson {
+
+   private ArrayList<Teacher> teachers;
+   private Classroom classroom;
+   private ArrayList<Class> studentClass;
+
+   public enum subject {
+
+      MATH,
+      PROGRAMMING,
+      POC,
+      WORKSHOP,
+      GRAPhICS2D,
+      GRAPHICS3D,
+      OOM,
+      HWI
+
+   }
+
+   public Lesson (ArrayList<Teacher> teachers, Classroom classroom, ArrayList<Class> studentClass) {
+
+      this.teachers = teachers;
+      this.classroom = classroom;
+      this.studentClass = studentClass;
+
+   }
+
+   public ArrayList<Teacher> getTeacher () {
+      return teachers;
+   }
+
+   public void setTeachers (ArrayList<Teacher> teacher) {
+      this.teachers = teacher;
+   }
+
+   public Classroom getClassroom () {
+      return classroom;
+   }
+
+   public void setClassroom (Classroom classroom) {
+      this.classroom = classroom;
+   }
+
+   public ArrayList<Class> getStudentClass () {
+      return studentClass;
+   }
+
+   public void setStudentClass (ArrayList<Class> studentClass) {
+      this.studentClass = studentClass;
+   }
+
+
 
 }
