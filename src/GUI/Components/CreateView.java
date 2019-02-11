350fb4f64d733737c41ef3f2661018b24ef4284c
Jeroen_Bozon <jjbozon@hotmail.nl>
2019-02-11 14:23:27 +0100

    CreateView changed
    Createview aangemaakt en veranderd

--- src/GUI/Components/CreateView.java
+++ src/GUI/Components/CreateView.java
@@ -1,19 +1,21 @@
 package GUI.Components;
 
+import Data.Agenda;
 import javafx.scene.layout.GridPane;
 
 import java.awt.*;
 import java.util.ArrayList;
 
 public class CreateView extends GridPane {
-    ArrayList<String> lessons = new ArrayList();
+    ArrayList<Data.Lesson> lessons = new ArrayList();
+    Agenda agenda = new Agenda(lessons);
     GridPane gridPane = new GridPane();
 
     public CreateView(){
         for(int amountLessons = 0; amountLessons< lessons.size(); amountLessons++){
-            Label label = new Label(lessons.get(amountLessons));
+            Label label = new Label("" + lessons.get(amountLessons));
             gridPane.addRow(amountLessons);
-            //gridPane.add(lessons.getTime(), 1, amountLessons);
+            //gridPane.add(lessons 1, amountLessons);
         }
 
     }
